c4ca87da115abad2e44876262dc3f614161e396d
Count Anthony Harrison <anthony@harrisonofthenorth.com>
2021-10-17 12:37:06 +0100

    Interim Refactoring

---
+++ Yatter.UI.ListBuilder/ListItems/WebPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Yatter.UI.ListBuilder.ListItems
+{
+    public class WebPage : TextBase, IDataType
+    {
+        public string Url { get; set; }
+
+        public WebPage()
+        {
+            DataType = GetType().ToString();
+        }
+    }
+}
+
