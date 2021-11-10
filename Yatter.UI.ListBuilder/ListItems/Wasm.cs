1fd6b5b2bc36da41cc1cd1d7f60e30f926f8c2de
Count Anthony Harrison <anthony@harrisonofthenorth.com>
2021-11-10 18:26:44 +0000

    Added Wasm DataType to ListBuilder

---
+++ Yatter.UI.ListBuilder/ListItems/Wasm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Yatter.UI.ListBuilder.ListItems
+{
+    public class Wasm : ListItemBase, IDataType
+    {
+        public new string DataType { get; set; }
+
+        public Wasm()
+        {
+            DataType = GetType().ToString();
+            Visibility = "visible";
+        }
+
+        public double? HeightWidthRatio { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public string? IndexBase64 { get; set; }
+        public string? WasmBase64 { get; set; }
+        public string? DataBase64 { get; set; }
+        public string? JavascriptBase64 { get; set; }
+    }
+}
+
